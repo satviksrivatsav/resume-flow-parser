182dbf086029292d5819c525b79b7366504ce7b8
satviksrivatsav <satviksrivatsav@gmail.com>
2026-05-22 01:32:57 +0530

    feat: update CORS configuration to disable credentials for improved security

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ if settings.LOGFIRE_TOKEN:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
